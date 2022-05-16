9e11b41e47236684cd58690980b4f089a2330c1c
Victor Bertoldo <eu_viktor@hotmail.com>
2022-05-16 17:50:22 +0000

    pivot table

---
+++ models/marts/core/int_orders__pivoted.sql
@@ -0,0 +1,23 @@
+{%- set payment_methods = ['bank_transfer', 'credit_card', 'gift_card'] -%}
+
+with 
+payments as (
+    select * from {{ ref('stg_payments') }}
+),
+pivoted as (
+    select 
+        order_id,
+        {% for payment_method in payment_methods -%}
+        sum(case when payment_method = '{{ payment_method }}' then amount else 0 end) as {{ payment_method }}_amount
+        {# {%- if loop.last != true -%} #}
+        {%- if not loop.last -%}
+        ,
+        {%- endif -%}        
+        {%- endfor -%}
+
+    from payments
+    where status = 'success'
+    group by 1
+)
+
+select * from pivoted
